7fe0d9d0a5a66fef7508d7b1ad56fcb405bafc9c
Brandon Ingli <brandon@brandoningli.com>
2020-02-25 10:31:49 -0600

    Body of Status Printer written
    Calls to get sharedData values may change once that definition is decided.

--- lb4theartbeat/HeartbeatStatusPrinter.java
+++ lb4theartbeat/HeartbeatStatusPrinter.java
@@ -1,9 +1,29 @@
 package lb4theartbeat;
-
+import java.util.ArrayList;
 public class HeartbeatStatusPrinter implements Runnable{
 
-  public void run(){
+  private HeartbeatSharedData sharedData;
 
+  public HeartbeatStatusPrinter(HeartbeatSharedData sharedData){
+    this.sharedData = sharedData;
+  }
+
+  public void run(){
+    while(!Thread.interrupted()){
+      ArrayList<Heartbeat> heartbeats = sharedData.getCache().values();
+      Date currentTime = new Date(System.currentTimeMillis());
+      System.out.println("Heartbeats as of " + currentTime);
+      System.out.println("================");
+      for(Heartbeat heartbeat : heartbeats){
+        System.out.println(heartbeat);
+      }
+
+      try{
+        Thread.sleep(sharedData.getMaxWait()*1000);
+      } catch (InterruptedException e){
+        break;
+      }
+    }
   }
   
 }
