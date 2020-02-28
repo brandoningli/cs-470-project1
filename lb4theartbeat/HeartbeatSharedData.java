5ff5560ac43ea830f5dc6a0a18113a161f0c4207
Chase Anderson <cma3582@truman.edu>
2020-02-28 15:47:34 -0600

    Added the modified HeartbeatSharedData file and changed the ignore file and the json file.

--- lb4theartbeat/HeartbeatSharedData.java
+++ lb4theartbeat/HeartbeatSharedData.java
@@ -1,5 +1,193 @@
 package lb4theartbeat;
 
+import java.net.DatagramSocket;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+/**
+ * Declares all variables for HeartbeatSharedData
+ * @author Chase Anderson
+ * @version 1.0
+ */
 public class HeartbeatSharedData {
+  private DatagramSocket socket;
+  private ArrayList<String> ipList;
+  private ArrayList<String> serverList;
+  private String IP;
+  private Hashtable<String, Heartbeat> heartHash;
+  private int TTL;
+  private int TTLMultiplier;
+  private boolean isServer;
+
+
+  /**
+   * Constructor for HeartbeatSharedData
+   * @param socket
+   * @param ipList
+   * @param serverList
+   * @param IP
+   * @param heartHash
+   * @param TTL
+   * @param TTLMultiplier
+   * @param isServer
+   */
+  public HeartbeatSharedData(DatagramSocket socket, ArrayList<String> ipList, ArrayList<String> serverList, String IP, Hashtable<String,Heartbeat> heartHash, int TTL, int TTLMultiplier, Boolean isServer) {
+    this.socket = socket;
+    this.ipList = ipList;
+    this.serverList = serverList;
+    this.IP = IP;
+    this.heartHash = heartHash;
+    this.TTL = TTL;
+    this.TTLMultiplier = TTLMultiplier;
+    this.isServer = isServer;
+  }
   
+
+  /**
+   * Gets the Socket
+   * @return socket
+   */
+  public DatagramSocket getSocket() {
+    return this.socket;
+  }
+
+  /**
+   * Sets the socket reference
+   * @param socket Socket reference
+   */
+  public void setSocket(DatagramSocket socket) {
+    this.socket = socket;
+  }
+  /**
+   * gets
+   * @return The array list of strings
+   */
+  public ArrayList<String> getIPList() {
+    return this.ipList;
+  }
+  /**
+   * Setter for IPlist 
+   * @param AR
+   */
+  public void setIPList(ArrayList<String> AR) {
+    this.ipList = AR;
+  }
+
+  /**
+   * Gets the list of servers in priority order 
+   * @return serverList
+   */
+  public ArrayList<String> getServerList() {
+    return this.serverList;
+  }
+  /**
+   * Sets the list of servers in priority order
+   * @param serverList
+   */
+  public void setServerList(ArrayList<String> serverList) {
+    this.serverList = serverList;
+  }
+
+  /**
+   * Gets the IP of the current machine
+   * @return IP
+   */
+  public String getIP() {
+    return this.IP;
+  }
+  /**
+   * Sets the IP of the current machine
+   * @param IP
+   */
+  public void setIP(String IP) {
+    this.IP = IP;
+  }
+  /**
+   * Gets the Hashtable of cached Heartbeats, mapped by IP address
+   * @return heartHash
+   */
+  public Hashtable<String,Heartbeat> getHeartHash() {
+    return this.heartHash;
+  }
+  /**
+   * Sets reference of current Heartbeat cache
+   * @param heartHash
+   */
+  public void setHeartHash(Hashtable<String,Heartbeat> heartHash) {
+    this.heartHash = heartHash;
+  }
+  /**
+   * Gets TTL
+   * @return TTL times the multiplier if this is the server, or just TTL otherwise
+   */
+  public int getTTL() {
+    
+    if (isServer()){
+     return this.TTL * this.TTLMultiplier;
+    }
+    else{
+    return this.TTL;
+    }
+  }
+  /**
+   * Sets TTL
+   * @param TTL
+   */
+  public void setTTL(int TTL) {
+    this.TTL = TTL;
+  }
+  /**
+   * Gets the multiplier for TTL if this machine is the server
+   * @return TTLMultiplier
+   */
+  public int getTTLMultiplier() {
+    return this.TTLMultiplier;
+  }
+  /**
+   * sets the multiplier for TTL if this machine is the server 
+   * @param TTLx
+   */
+  public void setTTLMultiplier(int TTLx) {
+    this.TTLMultiplier = TTLx;
+  }
+  /**
+   * Gets if this machine is the server
+   * @return isServer boolean
+   */
+  public boolean isServer() {
+    return this.isServer;
+  }
+  /**
+   * Sets the current boolean value for isServer
+   * @param isServer
+   */
+  public void setIsServer(boolean isServer) {
+    this.isServer = isServer;
+  }
+  /**
+   * Gets the max wait time until another Heartbeat is expected
+   * @return TTL - 1
+   */
+  public int getMaxWait()
+  {
+    return TTL - 1;
+  }
+  
+  /**
+   * String representation of HeartbeatSharedData
+   * @return a String of HeartbeatSharedData
+   */
+  public String toString() {
+    return "{" +
+      " socket='" + getSocket() + "'" +
+      ", ipList='" + getIPList() + "'" +
+      ", serverList='" + getServerList() + "'" +
+      ", IP='" + getIP() + "'" +
+      ", heartHash='" + getHeartHash() + "'" +
+      ", TTL='" + getTTL() + "'" +
+      ", TTLMultiplier='" + getTTLMultiplier() + "'" +
+      ", isServer='" + isServer() + "'" +
+      "}";
+  }
+
 }
